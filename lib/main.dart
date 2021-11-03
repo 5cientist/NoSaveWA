32276e9fdcb0d8fd29d91d1cba02a9e6f320f67a
5cientist <luk.ku@hotmail.com>
2021-11-04 00:14:42 +0530

    added version 2 , added app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:nosavewa/pages/homeview.dart';
 
+// import 'package:flutter_launcher_icons/android.dart';
+// import 'package:flutter_launcher_icons/constants.dart';
+// import 'package:flutter_launcher_icons/custom_exceptions.dart';
+// import 'package:flutter_launcher_icons/ios.dart';
+// import 'package:flutter_launcher_icons/main.dart';
+// import 'package:flutter_launcher_icons/utils.dart';
+// import 'package:flutter_launcher_icons/xml_templates.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,10 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-
-      // theme: ThemeData.dark(),
       color: Colors.deepOrangeAccent,
-
       debugShowCheckedModeBanner: false,
       home: HomeView(),
     );
