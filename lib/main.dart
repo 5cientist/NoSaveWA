e683a95a59c51c7724046e667f9ce9f264a71cdb
5cientist <luk.ku@hotmail.com>
2022-01-10 15:59:39 +0530

    fixed bug for phone number limit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nosavewa/pages/homeview.dart';
 
-// import 'package:flutter_launcher_icons/android.dart';
-// import 'package:flutter_launcher_icons/constants.dart';
-// import 'package:flutter_launcher_icons/custom_exceptions.dart';
-// import 'package:flutter_launcher_icons/ios.dart';
-// import 'package:flutter_launcher_icons/main.dart';
-// import 'package:flutter_launcher_icons/utils.dart';
-// import 'package:flutter_launcher_icons/xml_templates.dart';
-
 void main() {
   runApp(MyApp());
 }
--- lib/pages/homeview.dart
+++ lib/pages/homeview.dart
@@ -7,11 +7,11 @@ import 'package:google_fonts/google_fonts.dart';
 class HomeView extends StatelessWidget {
   final mobilenumber = TextEditingController();
   final typedText = TextEditingController();
-  final defaultCode = '+91';
+  // final defaultCode = '+91';
 
   void addMessage() {
     final _url =
-        'https://api.whatsapp.com/send/?phone=${defaultCode + mobilenumber.text}&text=${typedText.text}';
+        'https://api.whatsapp.com/send/?phone=${mobilenumber.text}&text=${typedText.text}';
     print(_url);
     launch(_url);
   }
@@ -20,76 +20,81 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.black26,
-        appBar: VxAppBar(
-          centerTitle: true,
-          title: Text(
-            "NoSaveWA",
-            style: GoogleFonts.hennyPenny(
-              color: Colors.lightGreenAccent,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
+          backgroundColor: Colors.black26,
+          appBar: VxAppBar(
+            centerTitle: true,
+            title: Text(
+              "NoSaveWA",
+              style: GoogleFonts.hennyPenny(
+                color: Colors.lightGreenAccent,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
             ),
           ),
-        ),
-        // drawer: Drawer(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Divider(),
-
-              TextField(
-                controller: mobilenumber,
-                cursorColor: Colors.greenAccent,
-                decoration: InputDecoration(
-                  enabled: true,
-                  border: OutlineInputBorder(),
-                  filled: false,
-                  fillColor: Colors.green,
-                  hintText: '871432xxxx',
-                  labelText: 'Enter your mobile number',
+          // drawer: Drawer(),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Divider(),
+                TextField(
+                  controller: mobilenumber,
+                  cursorColor: Colors.greenAccent,
+                  decoration: InputDecoration(
+                    enabled: true,
+                    border: OutlineInputBorder(),
+                    filled: false,
+                    fillColor: Colors.green,
+                    hintText: '+91871432xxxx',
+                    labelText: 'Enter your mobile number',
+                  ),
+                  // maxLength: 10,
+                  keyboardType: TextInputType.phone,
+                  textInputAction: TextInputAction.next,
                 ),
-                enableInteractiveSelection: true,
-                maxLength: 10,
-                keyboardType: TextInputType.phone,
-                textInputAction: TextInputAction.next,
-                autocorrect: true,
-              ),
-
-              SizedBox(
-                height: 5,
-              ),
-
-              VxCard("Enter your message".text.make()).red600.make().centered(),
-              Divider(),
-              //
-
-              TextFormField(
-                controller: typedText,
-                autofocus: true,
-                maxLength: 20,
-                decoration: InputDecoration(
-                  hintText: 'Type your measseage',
-                  alignLabelWithHint: true,
-                  labelText: 'Enter you meassage',
-                  hintMaxLines: 10,
-                  border: OutlineInputBorder(),
+                SizedBox(
+                  height: 5,
                 ),
-                maxLines: 10,
-                minLines: 10,
+                VxCard("Enter your message".text.make())
+                    .red600
+                    .make()
+                    .centered(),
+                Divider(),
+                TextFormField(
+                  controller: typedText,
+                  autofocus: false,
+                  maxLength: 20,
+                  decoration: InputDecoration(
+                    hintText: 'Type your measseage',
+                    alignLabelWithHint: true,
+                    labelText: 'Enter you meassage',
+                    hintMaxLines: 10,
+                    border: OutlineInputBorder(),
+                  ),
+                  maxLines: 10,
+                  minLines: 10,
+                ),
+                ElevatedButton(
+                  // color: Colors.green[800],
+                  onPressed: () {
+                    addMessage();
+                  },
+                  child: "Send".text.lg.make(),
+                ).p1(),
+              ],
+            ),
+          ),
+          bottomSheet: Align(
+            heightFactor: 0.5,
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              'Developed by Hanshir Lukku',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 18,
               ),
-
-              ElevatedButton(
-                // color: Colors.green[800],
-                onPressed: () {
-                  addMessage();
-                },
-                child: "Send".text.lg.make(),
-              ).p1(),
-            ],
-          ).centered(),
-        ),
-      ),
+            ),
+          )),
     );
   }
 }
